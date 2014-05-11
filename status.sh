840312c09bbd00bed98163902565872963d03f1f
Kjetil Valle <kjetil.valle@gmail.com>
2014-05-11 12:55:30 +0200

    fetching status remotely

--- status.sh
+++ status.sh
@@ -3,18 +3,41 @@
 green() { echo -e "\033[32m$1\033[0m"; }
 red() { echo -e "\033[31m$1\033[0m"; }
 
-for script in components/*.sh; do
-    if [ -x $script ]; then
-        msg=`./$script`
-        status=$?
-        if [ $status -gt 0 ]; then
-            red "$msg"
-        else
-            green "$msg"
+gather_locally() {
+    for script in components/*.sh; do
+        if [ -x $script ]; then
+            msg=`./$script`
+            status=$?
+            if [ $status -gt 0 ]; then
+                red "$msg"
+            else
+                green "$msg"
+            fi
         fi
+    done
+    unset script
+}
+
+gather_remote() {
+    if [[ $1 == "test" ]]; then
+        host="javabin@test.2014.javazone.no"
+    elif [[ $1 == "prod" ]]; then
+        host="javabin@2014.javazone.no"
+    else
+        echo "Unknown host '$1'. Leave blank to gather locally."
+        exit 1
     fi
-done
-unset script
+
+    echo "Status for: $host"
+    ssh $host "cd helsesjekk && ./status.sh"
+    exit $?    
+}
+
+if [ "$1" ]; then
+    gather_remote $1
+else
+    gather_locally
+fi
 
 # TODO
 
