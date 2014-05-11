448c31109cf2a860fb1bc4e244c532b163bd8199
Kjetil Valle <kjetil.valle@gmail.com>
2014-05-11 12:33:22 +0200

    logging errors from mongo

--- components/mongo.sh
+++ components/mongo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pid="$(mongo --quiet --eval 'db.serverStatus().pid;' 2>/dev/null)"
+pid="$(mongo --quiet --eval 'db.serverStatus().pid;' 2>details/MONGO.log)"
 status=$?
 
 if [ $status -gt 0 ]; then
