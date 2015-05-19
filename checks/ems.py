62ace8a8ff2ff24b36a39cebf5c25b8b7fc12dbc
Kjetil Valle <kjetil.valle@gmail.com>
2015-05-19 18:13:54 +0200

    Fixed health check for EMS

--- checks/ems.py
+++ checks/ems.py
@@ -5,14 +5,14 @@ from json import loads
 import requests
 
 try:
-    r = requests.get("http://localhost:8081/ems/server/app-info")
+    r = requests.get("http://localhost:9001/ems/server/app-info")
     if r.status_code >= 400:
         print "Status code %s" % r.status_code
         sys.exit(1)
 
-    data = r.json()
+    data = r.json()['build-info']
     print "Version: %(version)s" % data
-    print "Commit: %(git-sha)s" % data
+    print "Commit: %(sha)s" % data
 except:
     print "Unable to connect"
     sys.exit(1)
