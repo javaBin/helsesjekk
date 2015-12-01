5b4930ec1aa482e3634bf4ddf7fe567f9adc6a8d
Ingar Abrahamsen <ingar.abrahamsen@itera.no>
2015-12-01 19:33:40 +0100

    Improved devnull health check
    The origin health check did only check the http response code. The
    new one also pring the version, git hash and build time.

--- checks/devnull.sh
+++ checks/devnull.sh
@@ -1,5 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python
 
-. common.sh
+import sys
+from json import loads
+import requests
 
-check_curl_status "devnull" "http://127.0.0.1:9242/server/"
+try:
+#    env_host = "http://localhost:9001/"
+    envHost = "http://test.javazone.no/"
+    appInfo = envHost + "devnull/server/app-info"
+    r = requests.get(appInfo)
+    if r.status_code >= 400:
+        print "Status code %s" % r.status_code
+        sys.exit(1)
+
+    data = r.json()
+    print "Version: %(version)s" % data
+    print "Commit: %(sha)s" % data
+    print "Build time: %(buildTime)s" % data
+except:
+    print "Unable to connect"
+    sys.exit(1)
